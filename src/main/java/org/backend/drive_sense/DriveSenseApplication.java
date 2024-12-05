038da53cc4e9152fd835a41ec16304443a115878
habibaahmed11 <habibaahmed3372@gmail.com>
2024-12-05 13:58:10 +0200

    initial commit

---
+++ src/main/java/org/backend/drive_sense/DriveSenseApplication.java
@@ -0,0 +1,13 @@
+package org.backend.drive_sense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveSenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/backend/drive_sense/DriveSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.drive_sense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
